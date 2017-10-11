49332bd9ffd2d5e54c98c42cf74d36f9f0aefdbf
KarlEdwards <edwards.karl@ymail.com>
2017-10-11 16:47:26 -0700

    added images

--- functions_basic.R
+++ functions_basic.R
@@ -1,21 +1,18 @@
+## ---- inserted
 
-#' # Functions
-name_axes <- function( m ){
-  colnames( m ) <- axes
-  m
-}
+#+ basic_functions, echo = TRUE
 
 # Get the data; don't plot
-load_model <- function( filename ) name_axes( readSTL( filename, plot=FALSE ))
+load_data <- function( filename ) name_axes( readSTL( filename, plot=FALSE ))
 
 # Move the model into the corner of [ X, Y, Z ] = [ 0, 0, 0 ]
-center <- function( model ) name_axes( apply( model, 2, function(f) f - min( f )))
+##DELETE center <- function( model ) name_axes( apply( model, 2, function(f) f - min( f )))
 
 # Cut off the top/bottom, front/back, or left/right sides of the model
 clip_at <- function( obj, ax, mn, mx ) name_axes( obj[ obj[ ,ax ] > mn & obj[ ,ax ] < mx, ] )
 
 # Alternatively, clip a thin band out of the middle, and keep that portion that was removed
-get_band <- function( m=model, a=y_axis, b=0.5, w=stripe_width ) name_axes( clip_at( m, a, b - w, b + w ))
+get_band <- function( m=model_matrix, a=y_axis, b=0.5, w=STRIPE_WIDTH ) name_axes( clip_at( m, a, b - w, b + w ))
 
 # Show bounding box
 in_a_box <- function(){
@@ -29,41 +26,27 @@ in_a_box <- function(){
   )
 }
 
-# Show the model from a particular point of view
-see <- function( obj, viewpoint = 'oblique2', limits = c( 0, 1 ) ){
+view <- function( vp=cfg ) rgl.viewpoint(
+    theta = vp$get()$theta
+  , phi   = vp$get()$phi
+  , fov   = vp$get()$fov
+  , zoom  = vp$get()$zoom
+)
 
-  views  <- tribble(
-    # ________   _____   _____   _____   _____
-      ~name    , ~xrot , ~yrot , ~zrot , ~zoom
-    , 'oblique',   55  ,  -15  ,   45  ,   1
-    , 'oblique2', 175  ,  -15  ,   45  ,   1
-    , 'zy'     ,   90  ,  -0.5,    1  ,   1
-    , 'zx'     ,    0  ,   90  ,    1  ,   1
-    , 'xy'     ,    0.5,  -0.1,    0.5,   1
-  )
-  stopifnot( viewpoint %in% views[[ 'name' ]] )
-  view_parameters <- views[ views[ 'name' ] == viewpoint, ]
-
-  clear3d()                        # Start with nothing
-  bg3d( 'lightgray' )              # Set background color
-  plot3d(                          # Show the model
-      obj
-    , xlim = limits
-    , ylim = limits
-    , zlim = limits
-  )
-  points3d( 0, 0, 0, col = 'red' ) # Highlight the origin
-  axes3d()                         # Show the axes
-  in_a_box()                       # Show bounding box
-  text3d( x=0.75, y=0.75, z=0.75, text=paste0( 'View ', viewpoint ) ,col="Blue")
-
-  rgl.viewpoint(                   # Set the viewpoint
-      theta = view_parameters[ 2 ]
-    , phi   = view_parameters[ 3 ]
-    , fov   = view_parameters[ 4 ]
-    , zoom  = view_parameters[ 5 ]
-  )
+adjust <- function( cfg, parameter, val ){
+  cfg$set( parameter, val )
+  view( cfg )
 }
+
+background      <- function( bgcolor ) bg3d( bgcolor )
+see             <- function( obj, config, limits=c(0,1) ){
+  clear3d()
+  plot3d( obj, xlim = limits , ylim = limits , zlim = limits )
+  view( config )
+  background( 'white' )
+}
+reference_point <- function( coords=c(0,0,0), ref_color='red' ) points3d( coords[1], coords[2], coords[3], col = ref_color )
+move_it         <- function( obj, dx, dy, dz ) assign( obj, translate3d( obj=obj, x=dx, y=dy, z=dz ), envir = .GlobalEnv )
 
 make_figure <- function( caption ) rgl.snapshot( filename = paste0( caption, '.png' ))
 
@@ -75,9 +58,9 @@ best_slice <- function( model, ax ){
   biggest_slice <- which(h$counts==max(h$counts))
   h$mids[ biggest_slice ]
 }
-#best_x <- best_slice( model, x_axis )
-#best_y <- best_slice( model, y_axis )
-#best_z <- best_slice( model, z_axis )
+#best_x <- best_slice( model, X_AXIS )
+#best_y <- best_slice( model, Y_AXIS )
+#best_z <- best_slice( model, Z_AXIS )
 
 # Show rotation in grey
 transrotate <- function( obj, deg, u=0, v=0, w=0, x=0, y=0, z=0, color='darkgrey' ){
@@ -95,11 +78,11 @@ transrotate <- function( obj, deg, u=0, v=0, w=0, x=0, y=0, z=0, color='darkgrey
 }
 
 # Show slices in grey
-transslice <- function( obj, ax=y_axis, h=0.5, x=0, y=0, z=0, color='darkgrey' ){
+transslice <- function( obj, ax=Y_AXIS, h=0.5, x=0, y=0, z=0, color='darkgrey' ){
   limits <- c( 0, 1 )
   points3d(
     translate3d(
-        get_band( m=obj, a=ax,b=h*max(obj[,ax]), w=4*stripe_width )
+        get_band( m=obj, a=ax,b=h*max(obj[,ax]), w=3*STRIPE_WIDTH )
       , x=x, y=y, z=z
     )
     , col=color
@@ -107,4 +90,83 @@ transslice <- function( obj, ax=y_axis, h=0.5, x=0, y=0, z=0, color='darkgrey' )
     , ylim = limits
     , zlim = limits
   )
+}
+
+get_curve_points <- function( model_matrix, as_width='x', as_height='y', as_curve='z', tol = STRIPE_WIDTH, height, width, fun='min' ){
+  is_near <- function( x, a, delta ){
+    big_enough   <- x %>% map_lgl( ~ .x > a - delta )
+    small_enough <- x %>% map_lgl( ~ .x < a + delta )
+    big_enough & small_enough
+  }
+
+  # Return the max(min) value of as_curve in the box
+  # defined by as_width near width and as_height near height
+  cat(
+    sprintf(
+        '\nChecking ( %4.2f, %4.2f ) ...'
+      , height
+      , width
+    )
+  )
+  goodx  <- model_matrix[ is_near( model_matrix[ ,as_width ], width , tol ), ]
+  goodxy <- goodx[ is_near( goodx[ ,as_height], height, tol ), ]
+  if( nrow( goodxy ) == 0 ) return( Inf )
+  do.call( fun, as.list( goodxy[ ,as_curve ] ) )
+}
+
+get_minz_at_hx <- function( model, h, x, fun = 'min' ){
+  results <- get_curve_points(
+      model_matrix = model$get()
+    , as_width='x'
+    , as_height='y'
+    , as_curve='z'
+    , tol = 5 * STRIPE_WIDTH
+    , height = h
+    , width  = x
+    , fun = fun
+  )
+  c( h, x, results )
+}
+
+get_perpendicular2D <- function( m, P ){
+  if( m == 0 ){                             # perpendicular is vertical, i.e., slope undefined
+    result <-  list( 'slope' = NA, 'intercept' = NA )
+  } else {
+    normal_slope <- -1 / m
+    x <- P[ , 1 ]
+    y <- P[ , 2 ]
+    normal_intercept <- y - normal_slope * x
+    result <- list( 'slope' = normal_slope, 'intercept' = normal_intercept )
+  }
+  result
+}
+
+# For line intersection in 3D, see https://math.stackexchange.com/questions/28503/how-to-find-intersection-of-two-lines-in-3d
+get_intersection2D <- function( line1, line2 ){
+  x <- ( line2$intercept - line1$intercept ) / ( line1$slope - line2$slope )
+  y <- line1$slope * x + line1$intercept
+  list( 'x' = x, 'y' = y )
+}
+
+euclidean_distance <- function( P1, P2 ){
+  deltas <- unlist( matrix( P1, nrow = 1 ) ) - unlist( matrix( P2, nrow = 1 ) ) 
+  sqrt( sum( deltas^2 ))
+}
+
+get_intercept <- function( P, m ) P[ ,2 ] - m * P[ ,1 ]
+
+get_slope2D <- function( P1xy, P2xy ){
+  delta_x <- P2xy[ ,1 ] - P1xy[ ,1 ]
+  delta_y <- P2xy[ ,2 ] - P1xy[ ,2 ]
+  cat( sprintf( '\ndelta x: %5.4f',delta_x))
+  cat( sprintf( '\ndelta y: %5.4f',delta_y))
+  if( delta_x == 0 ){
+    slopeMsg <- "\nm: none"
+    sloveVal <- null
+  } else {
+    slopeMsg <- sprintf( "\nm: %3.2f", delta_y / delta_x )
+    slopeVal <- round( delta_y / delta_x, digits = 4 )
+  }
+  #noquote( slopeMsg )
+  slopeVal
 }
--- functions_contour.R
+++ functions_contour.R
@@ -1,3 +1,4 @@
+#+ contour_functions, echo = TRUE
 slice_the_model <- function( model=model, as_x='x', as_y='z', slice_on='y', n_slices = 20, tol = 0.0025 ){
   a <- min( model[ , slice_on ])
   b <- max( model[ , slice_on ])
@@ -23,7 +24,7 @@ slice_the_model <- function( model=model, as_x='x', as_y='z', slice_on='y', n_sl
 
 }
 
-# Calculate the data slices on x, y, and z
+# Calculate the data slices on x, y, and z one regular intervals
 # data <- slice_the_model( model, 'z', 'y', 'x', 16, 0.0025 )
 # data <- slice_the_model( model, 'x', 'z', 'y', 16, 0.0025 )
 # data <- slice_the_model( model, 'y', 'x', 'z', 16, 0.0025 )
@@ -32,11 +33,22 @@ get_slice_plots <- function( slice_data ){
   slice_plots <- seq_along( slice_data ) %>%
     map(
       ~ggplot(
-          data[[ .x ]]
+          slice_data[[ .x ]]
         , aes(x=x, y=y)
-        , main = paste( 'slice', as.character( .x ))
       ) +
-      geom_point( size=0.1 ) +
+      ggtitle(
+        paste(
+            'slice at '
+          , as.character(
+              round(
+                  100 * .x / length( slice_data )
+              )
+              , 2
+            )
+          , '%'
+        )
+      ) +
+      geom_point( size=0.1, na.rm = TRUE ) +
       xlim( 0, 1 ) +
       ylim( 0, 1 )
     )
@@ -45,3 +57,5 @@ get_slice_plots <- function( slice_data ){
   d <- ceiling( sqrt( length( slice_data)))
   marrangeGrob( slice_plots, nrow=d, ncol=d)
 }
+
+#ggsave( 'contours_x.png' )
