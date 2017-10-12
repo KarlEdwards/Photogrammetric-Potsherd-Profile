ff95a88667d31c0aaa1495af03de5dc003aff597
KarlEdwards <edwards.karl@ymail.com>
2017-10-12 16:26:59 -0700

    added more R source files

---
+++ R/critical_points.R
@@ -0,0 +1,82 @@
+critical_points <- function( data, delta ){
+  # Examine the slope of the segment defined by pairs of
+  # consecutive points (A and B), which are separated by
+  # a distance of at least delta. Identify relative minima
+  # and maxima.
+  N         <- nrow( data )
+
+  y         <- vector( mode = 'numeric', length = 0)
+  z         <- vector( mode = 'numeric', length = 0)
+  m         <- vector( mode = 'numeric', length = 0)
+  d         <- vector( mode = 'character', length = 0)
+
+  i         <- 1           # first row of results
+  point_A   <- 1           # begin with the first point in the data
+  point_B   <- 1 + point_A # try the next point
+  direction <- 'unknown'   # until the first segment is analyzed, direction is unknown
+  base_x    <- data[ 1, 1 ]
+  base_y    <- data[ 1, 2 ]
+
+  # Find the distance along the artifact axis of rotation between A and B
+  delta_y   <- function() data[ point_B , 'y' ] - data[ point_A, 'y' ]
+
+  # Find the distance along the artifact radius between A and B
+  delta_z   <- function() data[ point_B , 'z' ] - data[ point_A, 'z' ]
+
+  # Keep going until the last point has been examined
+  more_rows <- function() point_B < N
+
+  # Return rise / run, unless run is zero or rise is insignificant 
+  slope <- function(){
+    ifelse(
+      ( ( abs( delta_z() ) < delta ) | delta_y() == 0 )
+      , NA
+      , delta_z() / delta_y()
+    )
+  }
+
+  set_direction <- function(){
+    if( abs( delta_z() ) < delta ){
+      direction <<- 'flat'
+    } else {
+      # translate sign ( 1 or -1 ) into TRUE/FALSE
+      up <- 1 + sign( slope() )
+      direction <<- switch( direction
+        , unknown          = ifelse( up, 'rising'        , 'falling'        )
+        , flat             = ifelse( up, 'rising'        , 'falling'        )
+        , falling          = ifelse( up, 'ridge'       , 'falling'        )
+        , rising           = ifelse( up, 'rising'        , 'groove'        )
+        , groove          = ifelse( up, 'rising'        , 'shoulder_left'  )
+        , ridge          = ifelse( up, 'shoulder_right', 'falling'        )
+        , shoulder_right   = ifelse( up, 'rising'        , 'falling'        )
+        , shoulder_left    = ifelse( up, 'rising'        , 'falling'        )
+        , 'other'
+      ) # end switch
+    }
+  }
+
+  while( more_rows() ){
+
+    while( ( abs( delta_z() ) < delta ) & more_rows() ) point_B <- point_B + 1
+
+    if( abs( delta_z() ) >= delta ){
+      EMPIRICALLY_DETERMINED_MAGIC_NUMBER <- 0.25  # Why not 0.5 * distance between points, i.e., mean?
+      y[ i ] <- data[ point_A, 'y' ] + EMPIRICALLY_DETERMINED_MAGIC_NUMBER * ( data[ point_B, 'y' ] - data[ point_A, 'y' ] )
+      z[ i ] <- data[ point_A, 'z' ] - EMPIRICALLY_DETERMINED_MAGIC_NUMBER * ( data[ point_B, 'z' ] - data[ point_A, 'z' ] )
+##      y[ i ] <- data[ point_A, 'y' ]
+##      z[ i ] <- data[ point_A, 'z' ]
+      m[ i ] <- slope()
+      d[ i ] <- set_direction()
+
+      point_A <- point_B
+      point_B <- point_A + 1
+      i <- i + 1
+    }
+  } #end while
+
+  # return result
+##  results <- do.call( rbind, results )
+##  colnames( results ) <- c( 'y', 'z', 'slope', 'direction' )
+##  as.tibble( results )
+  data.frame( y = y, z = z, slope = m, direction = d )
+}
---
+++ R/functions_contour.R
@@ -0,0 +1,61 @@
+#+ contour_functions, echo = TRUE
+slice_the_model <- function( model=model, as_x='x', as_y='z', slice_on='y', n_slices = 20, tol = 0.0025 ){
+  a <- min( model[ , slice_on ])
+  b <- max( model[ , slice_on ])
+  slice_range <- b - a
+
+  nominal_contours <<-
+    seq( n_slices ) %>%
+      map_dbl( ~a + .x * slice_range / n_slices )
+
+  a_slice <- function( i ){
+    this <- model[ , slice_on ]
+    that <- nominal_contours[ i ]
+    fields <- c( as_x, as_y )
+    indecies <- abs( this - that ) < tol
+    df <- as.data.frame( model[ indecies, fields ] )
+    names( df ) <- c( 'x', 'y' )
+    df
+  }
+
+  actual_contours <-
+    seq( n_slices ) %>%
+      map( ~a_slice( .x ))
+
+}
+
+# Calculate the data slices on x, y, and z one regular intervals
+# data <- slice_the_model( model, 'z', 'y', 'x', 16, 0.0025 )
+# data <- slice_the_model( model, 'x', 'z', 'y', 16, 0.0025 )
+# data <- slice_the_model( model, 'y', 'x', 'z', 16, 0.0025 )
+
+get_slice_plots <- function( slice_data ){
+  slice_plots <- seq_along( slice_data ) %>%
+    map(
+      ~ggplot(
+          slice_data[[ .x ]]
+        , aes(x=x, y=y)
+      ) +
+      ggtitle(
+        paste(
+            'slice at '
+          , as.character(
+              round(
+                  100 * .x / length( slice_data )
+              )
+              , 2
+            )
+          , '%'
+        )
+      ) +
+      geom_point( size=0.1, na.rm = TRUE ) +
+      xlim( 0, 1 ) +
+      ylim( 0, 1 )
+    )
+
+  # Create a matrix of plots
+  d <- ceiling( sqrt( length( slice_data)))
+  marrangeGrob( slice_plots, nrow=d, ncol=d)
+}
+
+#ggsave( 'contours_x.png' )
